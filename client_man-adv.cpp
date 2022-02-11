3b5d6138ac1b02b51d858e096dd90215759b61b6
Evan Clegern <evanclegern.work@gmail.com>
2022-02-11 16:27:41 -0700

    Better testing code

--- client_man-adv.cpp
+++ client_man-adv.cpp
@@ -10,14 +10,18 @@ int main() {
 		c_Socket_TCP test2(1801, DO_WAIT, TCP_SETUP_CONNECT);
 		test2.connectionEstablish(them, 8100);
 		
-		char mesg[10] = "Shutdown!", mesg2[12] = "No shutdown";
+		char mesg[10] = "Shutdown!", mesg2[12] = "No shutdown", mesg3[15] = "I like server!";
 		test.transmit(mesg, 10, 1);
 		test2.transmit(mesg2, 12, 0);
 		test.shutdownSocket();
+		Clib::sleep(3);
+		test2.transmit(mesg3, 15, 1);
 		Clib::sleep(1);
 		test2.transmit(mesg, 10, 0);
 		test2.shutdownSocket();
-	} catch (std::exception B) {
-		std::cerr << B.what();
+	} catch (std::exception* B) {
+		std::cerr << B->what();
+		return 1;
 	}
+	return 0;
 }
